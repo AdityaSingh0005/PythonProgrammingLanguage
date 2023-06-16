cc641a3a8482b2775609094202a8f95ec31f2a51
Aditya SIngh <as8916909@gmail.com>
2023-06-16 17:49:05 +0530

    Initial Commit

--- Day10.py
+++ Day10.py
@@ -1,4 +1,27 @@
-name = "Aditya"
-friend = "Rohan"
+# Conditional Statement
+# If Else Statements
 
-print(name[0])
+# Conditional Operator
+# >, <, >=, <=, ==, !=
+# a = int(input("Enter Your age: "))
+# print("Your age is: ", a)
+
+# print(a>18)
+# print(a<18)
+# print(a<=18)
+# print(a>=18)
+# print(a==18)
+# print(a!=18)
+# if(a>18):
+#     print("You Can drive")
+# else:
+#     print("You cannot drive")
+    
+applePrice = 10
+budget = 200
+if (budget - applePrice > 50):
+    print("Alexa, add 1 kg Apples to the cart.")
+    
+else:
+    print("Alexa, do not add Apples to the cart.")
+    
