72d3fae0c7b65ee59b12f338712ca7c96e97d490
Aditya SIngh <as8916909@gmail.com>
2023-06-16 13:22:57 +0530

    Initial Commit

--- Day8.py
+++ Day8.py
@@ -1,7 +1,15 @@
 # String Slicing
+
 names = "Aditya, Shubham"
 fruit = "Mango"
 len1 = len(fruit)
 print(names[0:5])
 print(len(names))
 print("Mango is a", len1, "letter word.")
+print(len1) # for calculating lenth of the string 
+print(fruit[0:4])
+print(fruit[2:4])
+print(fruit[: len(fruit)-3])
+print(fruit[-3:1])
+nm = "Harry"
+print(nm[-4:-2])
