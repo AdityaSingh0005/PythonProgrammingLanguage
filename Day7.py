2f3dae4afbae722aede89db27d0525ee4a8816ce
Aditya SIngh <as8916909@gmail.com>
2023-06-16 12:57:29 +0530

    Intial Commit

--- Day7.py
+++ Day7.py
@@ -1,10 +1,20 @@
 # String
 
-name = "harry"
+name = "Aditya"
 friend = "aditya"
 anotherFriend = 'Lovish'
 apple = 'He said, \n I want to eat an apple'
 
+fruit = '''This is an apple'''
+
 print("Hello, " + friend)
 print("Hello, " + str(friend))
-print(apple)
+# print(apple)
+print(name[0])
+print(name[1])
+print(name[5])
+# print(name[6]) This Throws an error 
+print("HEllo, " + str(fruit))
+print("Lets use a for loop\n")
+for character in name:
+    print(character)
